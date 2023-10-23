09556dc2ebd43ac4b0ed4d043f6f74e03e9534d1
haidyi <haidyi@cs.unc.edu>
2023-10-22 23:03:17 -0400

    fix bug for docs

--- src/sclkme/__init__.py
+++ src/sclkme/__init__.py
@@ -1,5 +1,8 @@
+# has to be done at the end, after everything has been imported
+import sys
+
 from . import io
 from . import tools as tl
 from . import utils
 
-__all__ = ["tl", "io", "utils"]
+sys.modules.update({f"{__name__}.{m}": globals()[m] for m in ["tl"]})
